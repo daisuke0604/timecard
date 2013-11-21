80954a9b2e0548d2b0f81c2d32ef354bef9827eb
Daisuke Yamamoto <dqa03151@gmail.com>
2013-11-21 15:56:22 +0900

    usersテーブルのテスト

--- test/test_users.js
+++ test/test_users.js
@@ -1,14 +1,28 @@
 var users = require('../users');
 var should = require('should');
 
-describe('users test', function() {
+describe('userテーブルのテスト', function() {
+
+    it('ユーザ追加', function(done) {
+        var newuser = new users.User("test@test.co.jp", "テスト 太郎", "pass", 0);
+        users.insert(newuser, function(err) {
+            should.not.exists(err);
+            done();
+        });
+    });
     
-    it('ユーザ追加', function() {
-        users.insert("test@test.co.jp", "テスト 太郎", "pass", 0);
+    var user = new users.User();
+    it('ユーザ検索', function(done) {
+        users.findById('test@test.co.jp', user, function(err) {
+            should.not.exists(err);
+            done();
+        });
     });
 
-    it('ユーザ検索', function() {
-        var user = users.findById("test@test.co.jp");
+    it('値の検証', function() {
+        should.exists(user);
+        should.equal(user.mail, 'test@test.co.jp');
+        should.equal(user.name, 'テスト 太郎');
     });
 
 });
--- users.js
+++ users.js
@@ -1,18 +1,33 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('db/db.sqlite3');
 
+// User object
+var User = function(mail, name, password, option) {
+    this.mail = mail;
+    this.name = name;
+    this.password = password;
+    this.option = option;
+};
+exports.User = User;
+
 // Add record
-exports.insert = function(mail, name, password, option) {
+exports.insert = function(user, opt_callback) {
     db.serialize(function() {
-        db.run("INSERT INTO users VALUES (?, ?, ?, ?)", mail, name, password, option);
+        db.run("INSERT INTO users VALUES (?, ?, ?, ?)", user.mail, user.name, user.password, user.option, function(err) {
+            if (opt_callback) opt_callback(err || null);
+        });
     });
 };
 
 // Find record by mail
-exports.findById = function(mail) {
+exports.findById = function(mail, user, opt_callback) {
     db.serialize(function() {
         db.get("SELECT * FROM users WHERE mail = ?", mail, function(err, row) {
-            return row;
+            user.mail = row.mail;
+            user.name = row.name;
+            user.password = row.password;
+            user.option = row.option;
+            if (opt_callback) opt_callback(err || null);
         });
     });
 };
