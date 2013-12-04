86adb3aa8314830b72a9739dbf0fa7f5b6247a27
Daisuke Yamamoto <dqa03151@gmail.com>
2013-12-04 15:33:29 +0900

    index.ejs表示まで

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path');
+var express = require('express'),
+    routes = require('./routes'),
+    http = require('http'),
+    path = require('path');
 
 // ハッシュ値計算準備
 var crypt = require('crypto');
@@ -10,24 +10,24 @@ var getHash = function(target) {
     var sha = crypto.createHmac("sha256", secretKey);
     sha.update(target);
     return sha.digest("hex");
-}
+};
 
 // Passport準備
-var flash = require('connect-flush')
-  , passport = require('passport')
-  , LocalStrategy = require('passport-local').strategy;
-
+var flash = require('connect-flash'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy;
 
 // Passportでのセッション設定
 passport.serializeUser(function(user, done) {
+    // TODO シリアライズ対象のみ
     done(null, user);
 });
 passport.deserializeUser(function(obj, done) {
     done(null, obj);
 });
 
-var users = require("users"),
-    User = require("user");
+var users = require("./users"),
+    User = require("./user");
 
 var user = new User();
 
@@ -50,3 +50,63 @@ passport.use(new LocalStrategy(
         });
     }
 ));
+
+// ログイン済みかどうか
+var isLogined = function(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    // 未ログイン
+    res.redirect("/login");
+};
+
+var app = express();
+
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session({secret: "benestbenestbenest"}));
+
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(app.router);
+app.use(express.static(path.join(__dirname,  'public')));
+
+// development only
+if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+
+app.get("/login", function(req, res){
+    res.render("login", {user: req.user, message: req.flash("error")});
+});
+app.post("/login",
+    passport.authenticate("local", {failureRedirect: '/login', failureFlash: true}),
+    function(req, res){
+        // ログインに成功したらトップへリダイレクト
+        res.redirect("/");
+    }
+);
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+app.get("/member_only", isLogined, function(req, res){
+    res.render("member_only", {user: req.user});
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
